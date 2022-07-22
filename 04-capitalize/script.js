3b07834d21743d6b9bcce4711a441740713660b6
NataliyaNikulshina <victory515@yandex.ru>
2022-07-22 18:04:06 +0300

    feat:решена четвертая задача

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -7,9 +7,25 @@
 */
 
 function capitalize(str) {
-    // Напишите код здесь
+    str = str.replace(/\s+/g, ' ');
+    const arr = str.split(' ');
+    //console.log(arr);
+    const arrUp = arr.map(function(strUp){
+        if (!(strUp === '')){
+        strUp = strUp[0].toUpperCase() + strUp.slice(1);
+        return strUp;}
+    });
+
+    return arrUp.join(' ');
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(capitalize('молодость всё простит')); // "Молодость Всё Простит"
+console.log(capitalize('лето снаружи. лето внутри.'));
+console.log(capitalize('одно и то же в разном возрасте видится по-разному'));
+console.log(capitalize('Самой крупной рыбой в реке становится та, что не клюет на приманку'));
+console.log(capitalize('молодость  всё  простит'));
+console.log(capitalize(' '));
+console.log(capitalize('слово '));
+console.log(capitalize(''));
